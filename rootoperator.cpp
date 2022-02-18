6cf51c518a3bd0c450c88f65ce4a9476c7c41a9f
Anish Pillai <anishp8914@gmail.com>
2022-02-18 20:44:01 +0530

    Square Root Function Added

---
+++ rootoperator.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+# include <math.h>
+using namespace std;
+
+
+
+
+int main(){
+    float sq,n;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<endl;
+
+    sq=sqrt(n);
+
+    cout<<"The square root of "<<n<<" is "<<sq;
+
+    return 0; 
+
+
+}
